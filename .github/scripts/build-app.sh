f10d2ee722c33ae3aa2fce74ac8fc900c91445a9
griggheo <grig@gheorghiu.net>
2023-07-08 12:25:27 -0700

    install node and npm in build script

--- .github/scripts/build-app.sh
+++ .github/scripts/build-app.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+# install node and npm
+pushd /tmp
+curl -sL https://deb.nodesource.com/setup_18.x -o nodesource_setup.sh \
+	&& bash nodesource_setup.sh \
+	&& rm nodesource_setup.sh \
+	&& apt-get install -y nodejs --no-install-recommends
+popd
+
 make all
