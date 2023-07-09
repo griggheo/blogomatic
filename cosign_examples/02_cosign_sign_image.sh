80fb57049510f104bcb45be7d618b970e43fa41c
Grig Gheorghiu <grig@gheorghiu.net>
2023-07-09 06:19:11 +0000

    updated cosign examples

--- cosign_examples/02_cosign_sign_image.sh
+++ cosign_examples/02_cosign_sign_image.sh
@@ -2,5 +2,6 @@
 
 COSIGN_PRIVATE_KEY=$HOME/.cosign/cosign.key
 IMAGE=timoniersystems/blogomatic:distroless-multistage-2f07e41
+IMAGE=ghcr.io/codepraxis-io/blogomatic:9a4f7786-distroless
 
-echo 'y' | COSIGN_PASSWORD=$(cat ~/.k) cosign sign --key $COSIGN_PRIVATE_KEY "$IMAGE"
+echo "y" | COSIGN_PASSWORD=$(cat ~/.k) cosign sign --key $COSIGN_PRIVATE_KEY "$IMAGE"
--- cosign_examples/04_cosign_verify_signature.sh
+++ cosign_examples/04_cosign_verify_signature.sh
@@ -2,6 +2,7 @@
 
 COSIGN_PUBLIC_KEY=$HOME/.cosign/cosign.pub
 IMAGE=timoniersystems/blogomatic:distroless-multistage-2f07e41
+IMAGE=ghcr.io/codepraxis-io/blogomatic:9a4f7786-distroless
 
 cosign verify --key $COSIGN_PUBLIC_KEY "$IMAGE"
 
